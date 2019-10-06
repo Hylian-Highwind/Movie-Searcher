b3f3e5cf873893ffc9e8e91a41feb1d5791a4e53
Kyle Ring <kringabel@gmail.com>
2019-10-06 15:01:18 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,33 +1,30 @@
 <?php
     session_start();
 
-    if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
-        header("Location: alreadyin.php");
-    }
-        
     $dbhost = 'us-cdbr-iron-east-05.cleardb.net:3306';
     $dbuser = 'ba34f3f8d9d386';
     $dbpass = '6206b3d7';
     $dbname = 'heroku_f4436271c441c5d';
     $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
 
+    if(isset(['username'])){
+        $gaveuname = $_POST['username'];
+        $gavepsw = $_POST['password'];
 
-    $gaveuname = $_POST['username'];
-    $gavepsw = $_POST['password'];
+        //Make a Query to select one user from siteusers table in database that has a username and password matching the ones given by the form
+        $sql_query = "select Count(*) from siteusers WHERE username= '".$gaveuname."'AND user_password='".$gavepsw."'";
 
-    //Make a Query to select one user from siteusers table in database that has a username and password matching the ones given by the form
-    $sql_query = "select Count(*) from siteusers WHERE username= '".$gaveuname."'AND user_password='".$gavepsw."' Limit 1";
-
-    //capture the result of the query
-    $result = mysqli_query($conn, $sql_query);
-    
-    //If result has a row, it found a user with matching credentials
-    if($result == 1){
-        $_SESSION['logged_in'] = true;
-        header ("Location: loginsuccess.php");
-    }
-    else{
-        header ("Location: login.php");
+        //capture the result of the query
+        $result = mysqli_query($conn, $sql_query);
+        
+        //If result has a row, it found a user with matching credentials
+        if($result === 1){
+            $_SESSION['logged_in'] = true;
+            header ("Location: loginsuccess.php");
+        }
+        else{
+            header ("Location: login.php");
+        }
     }
 ?>
 

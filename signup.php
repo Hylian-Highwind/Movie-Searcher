c1b64fb04091caa491122d6dd008d8e3c5dca1e0
Kyle Ring <kringabel@gmail.com>
2019-10-06 16:53:08 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -40,11 +40,14 @@
          if(mysqli_query($conn, $sql)){
              echo "Account Created successfully.";
              echo '<a href ="login.php"> Click Here to Login </a>'
-         } else{
+         } 
+         /*
+         else{
              echo "ERROR: Unable to execute $sql. " . mysqli_error($conn);
              echo '<a href ="signup.php"> Try Signing Up Again </a>';
              echo '<a href ="index.php"> Back to Homepage </a>';
          }
+         /*
      }
  }
 ?>

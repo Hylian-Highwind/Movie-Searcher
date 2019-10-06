9edd8794f7ec2c904b19f879740a4ed74aafb104
Kyle Ring <kringabel@gmail.com>
2019-10-06 13:52:19 -0400

    Changing code to try new method from tutorial

--- login.php
+++ login.php
@@ -1,40 +1,33 @@
 <?php
+    session_start();
+    $username = "kring";
+    $password = "ringabel";
 
-    // Include config file
-    require_once "DBconfig.php";
- 
-    // Define variables and initialize with empty values
-    $username = $password  = "";
-    $username_err = $password_err = "";
-
-    // Process the Form Data submitted
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        if(empty(trim($_POST["username"]))){
-            $username_err = "Please enter a username.";
-        }
-        // Validate password
-        if(empty(trim($_POST["password"]))){
-            $password_err = "Please enter a password.";     
-        } else{
-            $password = trim($_POST["password"]);
+    if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true){
+        header("Location: index.php");
+    }
+    
+    if(isset($_POST['username']) && isset($_POST['password']) ){
+        if($_POST['username'] == $username && $_POST['password'] == $password){
+            $_SESSION['logged_in'] = true;
+            header ("Location: index.php");
         }
     }
-    // Check that input is not empty
-    if(empty($username_err) && empty($password_err) ){
-        
-        // Select user from the siteusers table where the entered username and password both match one for a registered user
-        $usermatch = Select * from siteusers where ($username = username && $password = user_password);
-
-        // Attempt to Redirect to Homepage if login successful
-        if($usermatch){
-            // Redirect to login page
-            header("location: index.html");
-        } else{
-            echo "Something went wrong. Please try again later.";
-        }
-         
-        // Close statement
-        mysqli_stmt_close($stmt);
-    }    
-
 ?>
+
+<!DOCTYPE HTML>
+
+<html>
+
+    <body>
+        <form>
+            Username:<br/>
+            <input type="text" placeholder="Enter Username" name="username" required> <br/>
+            Password: <br/>
+            <input type="password" placeholder="Enter Password" name="password" required> <br>
+            
+            <button type="submit">Login</button>
+        </form>
+    </body>
+
+</html>

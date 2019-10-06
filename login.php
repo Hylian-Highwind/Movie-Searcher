bd2ea8eb29ce71080189ae92ca5488b5486258fb
Kyle Ring <kringabel@gmail.com>
2019-10-06 15:10:40 -0400

    Update login.php

--- login.php
+++ login.php
@@ -16,7 +16,7 @@
 
         //capture the result of the query
         $result = mysqli_query($conn, $sql_query);
-        echo $result;
+        echo strval($result);
         //If result has a row, it found a user with matching credentials
         if($result === 1){
             $_SESSION['logged_in'] = true;

b346a156a31bb28a1ac01b8fea46fc5aadde8b17
Kyle Ring <kringabel@gmail.com>
2019-10-06 11:33:00 -0400

    Update Index.php
    Trying to add link to another Movie page with text input for part of the link

--- index.php
+++ index.php
@@ -1,15 +1,35 @@
 <?php ?>
 
+<!DOCTYPE html>
 <html>
 <head>
 
+    <link rel="stylesheet" type="text/css" href="mystylesheet.css">
+
+    <style>
+    
+    </style>
 </head>
 
 <body>
 
 <h1> Hello World! </h1>
-  <img src="Avengers.jpeg" alt ="Avengers (2012) Movie Poster" width = "200">
 
+<div class = "Wrapper" style="text-align: center">
+    <div class="Homepage-Search-Area">
+    <p> <font size = 6 face="Arial Black"> Welcome to the homepage. Here you can search through movies. </font></p>
+
+    <a href="Avengers.html"> Go to Avengers Page for Testing </a> <br>
+
+    <label for="Movie_Search" >Search for a movie</label><br>
+    <input type="text" id="Movie_Search" value="Avengers"/> <br>
+    <a href='' onclick = "this.href = document.getElementById('Movie_Search').value +'.html'">Go to Movie's Page</a>
+    </div>
+</div>
+
+<div class="Homepage-Movie-List">
+    <h3>Movies</h3>
+</div>
 </body>
 
 </html>

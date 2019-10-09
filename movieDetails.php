2e6e4d6e711fee3bc30c197d2cf70bbd5308f0ab
Kyle Ring <kringabel@gmail.com>
2019-10-09 11:06:47 -0400

    Update for variable warnings and button displays

--- movieDetails.php
+++ movieDetails.php
@@ -6,7 +6,6 @@
     //The name of the page, which should match this movie's movie_title in the movies Table
     $page = $_GET['title'];
 
-
     //Find the Movie associated with this Page in the movies table
     $sql_query = "select * from movies WHERE movie_title= '".$page."' limit 1";
     $result = mysqli_query($conn, $sql_query);
@@ -45,10 +44,10 @@
     
     //Handles Review Submission
     if(isset ($_POST['reviewSubmit']) ){
-        echo "Review submitted <br>";
+
         //Only allow a user to submit a review if they are logged in
         if(isset($_SESSION['logged_in'])){            
-            echo "Logged in <br>";
+
             $user = $_POST['username'];
             $score = $_POST['score'];
             $comment = $_POST['comment'];
@@ -57,10 +56,10 @@
             $sql_query = "select * from movie_reviews WHERE username= '".$user."' AND movie_id= '".$movieid."' limit 1";
             //capture the result of the query
             $result = mysqli_query($conn, $sql_query);
-            echo mysqli_num_rows($result);
             //If result has a row, this user has submitted a review already
             if(mysqli_num_rows($result) > 0){
                 echo "Found a review by you already";
+                $reviewedAlready = "<h2>Found a review by you already!</h2>";
             }
             else{
                 //Insert into the movie_reviews table (username, movie_id, score, movie_comment)
@@ -97,65 +96,99 @@
         function goBack() {
             window.history.back()
         }
-</script>
+    </script>
 </head>
 
 <body>
+<div class="Wrapper">
+    <div class = "Movie-Stuff">
+        <img src=<?php echo $poster ?> class = "Page-Poster">
+        <div class = "Movie-Info">
+            <!-- Display the title by echoing the title variable retrieved from the database-->
+            <p class = "Info-Title"> Title: <?php echo $title ?></p>
+            <p class="Synopsis"> <?php echo $synopsis ?> </p>
+            <p class = "Score"> Average Score: <?php echo $averagescore ?>/5</p>
+            
+        </div>
 
-<div class = "Movie-Stuff">
-    <img src=<?php echo $poster ?> class = "Page-Poster">
-    <div class = "Movie-Info">
-        <!-- Display the title by echoing the title variable retrieved from the database-->
-        <p class = "Info-Title"> Title: <?php echo $title ?></p>
-        <p class="Synopsis"> <?php echo $synopsis ?> </p>
-        <p class = "Score"> Average Score: <?php echo $averagescore ?>/5</p>
-        
     </div>
-
 </div>
 
-<div class ="Review-Section">
-<button onClick="location.href='index.php'" value='Home'>Home</button>
-    <div class="Wrapper">
-        <div id="reviewSubmissionDiv">
-        <form action= "" 
-        id = "reviewForm" Method = "POST">
-            <input type="hidden" name="username" value= <?php echo $_SESSION["username"] ?> >
+<div class="Wrapper">
+
+    <div class ="Review-Section">
+        <button class="RedirectButton" onClick="location.href='index.php'" value='Home'>Home</button>
+
+        
+        <div class="Wrapper">
+            <div id="reviewSubmissionDiv">
             
-            <label>Score</label> <br>
-            <div id="scoreEnterDiv">
-            <input id ="scoreEntry" type="number" name="score" min="1" max="5"> <span style ="font-size: 1.25em; font-weight: bold">/5</span>
+            <!--Display Duplicate review message if username already has one-->
+            <?php if(isset($reviewedAlready)){
+                    echo $reviewedAlready;
+                }
+                
+                ?>
+
+            <form action= "" 
+            id = "reviewForm" Method = "POST">
+                <input type="hidden" name="username" value= 
+                
+                <?php if(isset($_SESSION['logged_in'])){
+                    echo $_SESSION['username'];
+                }
+                
+                ?> >
+                
+                <label>Score</label> <br>
+                <div id="scoreEnterDiv">
+                <input id ="scoreEntry" type="number" name="score" min="1" max="5"> <span style ="font-size: 1.25em; font-weight: bold">/5</span>
+                </div>
+                <br>
+                <br>
+                <textarea name="comment"></textarea><br>
+                <!--<button type="submit" name ="reviewSubmit"> Post Review! </button> -->
+                <?php 
+                if(isset($_SESSION['logged_in'])){
+                    echo "<button type='submit' class = 'RedirectButton' name ='reviewSubmit'> Post Review! </button>";
+                }
+                else{
+                    echo "<button type = 'button' class = 'RedirectButton' onClick=location.href='login.php' value='Login'>Must Login to Review!</button>";
+                }
+                
+                ?>
+                
+
+            </form>
+
             </div>
-            <br>
-            <br>
-            <textarea name="comment"></textarea><br>
-            <button type="submit" name ="reviewSubmit"> Post Review! </button>
-        </form>
-
         </div>
-    </div>
-    <br>
-    <table class = "Review-Table">
-        <thead>
-            <th>Username</th>
-            <th>Review Score</th>
-            <th>Comment</th>
-        </thead>
-        <tbody>
-            <?php 
-                //Query for the reviews with an id matching this movie's
-                $sql_reviews_query = "Select * from movie_reviews WHERE movie_id = '".$movieid."' ORDER BY username";
-                $review_query_results = mysqli_query($conn, $sql_reviews_query);
-                while($review = mysqli_fetch_assoc($review_query_results) ){ ?>
-                    <tr>
-                    <td><?php echo $review['username'];?></td>
-                    <td><?php echo $review['score'];?></td>
-                    <td><?php echo $review['movie_comment'];?></td>
-                    </tr>
-               <?php  } ?>
-        </tbody>
+        <br>
 
-    </table>
+        <!--Table for outputting the info from reviews-->
+        <table class = "Review-Table">
+            <thead>
+                <th>Username</th>
+                <th>Review Score</th>
+                <th>Comment</th>
+            </thead>
+            <tbody>
+                <?php 
+                    //Query for the reviews with an id matching this movie's
+                    $sql_reviews_query = "Select * from movie_reviews WHERE movie_id = '".$movieid."' ORDER BY username";
+                    $review_query_results = mysqli_query($conn, $sql_reviews_query);
+                    while($review = mysqli_fetch_assoc($review_query_results) ){ ?>
+                        <tr>
+                        <td><?php echo $review['username'];?></td>
+                        <td><?php echo $review['score'];?></td>
+                        <td><?php echo $review['movie_comment'];?></td>
+                        </tr>
+                <?php  } ?>
+            </tbody>
+
+        </table>
+
+    </div>
 
 </div>
 
